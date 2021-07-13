55487768eaeb52af336fb66bfe652d642b724706
Plume <pluuumie@gmail.com>
2021-07-13 16:51:49 +0200

    refactor: added new functions and better syntax

--- src/ts/sprinkles.ts
+++ src/ts/sprinkles.ts
@@ -1,64 +1,76 @@
-export class Sprinkles {
-  selector: string;
-  options: object;
+class Sprinkles {
+  private selector: string;
+  private options: object = {
+    content: ['✨'],
+    amount: 20,
+    distance: 100,
+  };
+
   constructor(selector: string, options: object) {
     this.selector = selector;
     this.options = options;
-
-    let els = document.querySelectorAll(this.selector);
-
-    els.forEach((el) => {
-      el.addEventListener("click", () => {
-        // Clear old instance
-
-        if (el.querySelector("[data-sprinkles]")) {
-          el.querySelector("[data-sprinkles]").remove();
-        }
-
-        // Sprinkles wrapper
-
-        let sprinkles = document.createElement("div");
-        sprinkles.dataset.sprinkles = "";
-
-        // Create sprinkle wrapper inside
-
-        for (let i = 0; i < (this.options as any).amount; i++) {
-          let sprinkleContainer = document.createElement("div");
-          sprinkleContainer.dataset.sprinkleContainer = "";
-
-          let sprinkle = document.createElement("div");
-          sprinkle.dataset.sprinkle = "";
-          sprinkle.innerText = (this.options as any).content[
-            Math.floor(Math.random() * (this.options as any).content.length)
-          ];
-
-          sprinkleContainer.appendChild(sprinkle);
-          sprinkles.appendChild(sprinkleContainer);
-        }
-        el.appendChild(sprinkles);
-
-        // Get all sprinkles
-
-        let allSprinkles = el.querySelectorAll("[data-sprinkle]");
-
-        // Animate
-
-        requestAnimationFrame(() =>
-          requestAnimationFrame(() => {
-            allSprinkles.forEach((sprinkle: HTMLElement) => {
-              sprinkle.style.transform = `translateX(${this.randomCoord()}px) translateY(${this.randomCoord()}px) scale(4)`;
-              sprinkle.style.opacity = "0";
-            });
-          })
-        );
-      });
-    });
+    this.init();
   }
 
-  public randomCoord() {
+  private init = () => {
+    const targets = document.querySelectorAll(this.selector);
+    targets.forEach((target: HTMLElement) => {
+      this.wrapEl(target);
+    });
+  };
+
+  private wrapEl = (el: HTMLElement) => {
+    el.addEventListener('click', () => {
+      let oldSprinkles = el.querySelector('[data-sprinkles]');
+
+      if (oldSprinkles) {
+        oldSprinkles.remove();
+      }
+
+      let sprinkleWrapper = document.createElement('div');
+      sprinkleWrapper.dataset.sprinkles = '';
+
+      [...Array((this.options as any).amount)].map(() => {
+        let sprinkleContainer = document.createElement('div');
+        sprinkleContainer.dataset.sprinkleContainer = '';
+
+        let sprinkle = document.createElement('div');
+        sprinkle.dataset.sprinkle = '';
+        sprinkle.innerText = (this.options as any).content[
+          Math.floor(Math.random() * (this.options as any).content.length)
+        ];
+        
+        sprinkleContainer.appendChild(sprinkle);
+        sprinkleWrapper.appendChild(sprinkleContainer);
+      });
+
+      el.appendChild(sprinkleWrapper);
+
+      let sprinkles = [...(el.querySelectorAll('[data-sprinkle]') as any)];
+      this.animate(sprinkles);
+    });
+  };
+
+  private animate = (sprinkles: any) => {
+    requestAnimationFrame(() =>
+      requestAnimationFrame(() => {
+        sprinkles.map((sprinkle: HTMLElement) => {
+          sprinkle.style.transform = `
+          translateX(${this.randomCoord()}px) 
+          translateY(${this.randomCoord()}px) 
+          scale(4)`;
+          sprinkle.style.opacity = '0';
+        });
+      }),
+    );
+  };
+
+  private randomCoord() {
     return (
       (Math.ceil(Math.random() * (this.options as any).distance) + 1) *
       (Math.round(Math.random()) ? 1 : -1)
     );
   }
 }
+
+export default Sprinkles;
